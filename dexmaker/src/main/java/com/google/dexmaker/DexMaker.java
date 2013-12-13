94b42a39a70ec04de14412c6a8e124536a3d074a
Nandor Kracser <bonifaido@gmail.com>
2013-12-13 19:34:30 +0100

    fixing original dexmaker Issue 23: Class loading extension

--- dexmaker/src/main/java/com/google/dexmaker/DexMaker.java
+++ dexmaker/src/main/java/com/google/dexmaker/DexMaker.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import static java.lang.reflect.Modifier.PRIVATE;
 import static java.lang.reflect.Modifier.STATIC;
@@ -323,15 +324,41 @@ public final class DexMaker {
         }
     }
 
+    static File guessDexCache() {
+        String property = System.getProperty("dexmaker.dexcache");
+        File dexCache;
+        if (property != null) {
+            dexCache = new File(property);
+        } else {
+            dexCache = new AppDataDirGuesser().guess();
+            if (dexCache == null) {
+                throw new IllegalArgumentException("dexcache == null (and no default could be"
+                        + " found; consider setting the 'dexmaker.dexcache' system property)");
+            }
+        }
+        return dexCache;
+    }
+
+    static File spitToJar(File dexCache, byte[] dex) throws IOException {
+        File result = File.createTempFile("Generated", ".jar", dexCache);
+        result.deleteOnExit();
+        JarOutputStream jarOut = new JarOutputStream(new FileOutputStream(result));
+        jarOut.putNextEntry(new JarEntry(DexFormat.DEX_IN_JAR_NAME));
+        jarOut.write(dex);
+        jarOut.closeEntry();
+        jarOut.close();
+        return result;
+    }
+
     /**
      * Generates a dex file and loads its types into the current process.
-     *
+     * <p/>
      * <h3>Picking a dex cache directory</h3>
      * The {@code dexCache} should be an application-private directory. If
      * you pass a world-writable directory like {@code /sdcard} a malicious app
      * could inject code into your process. Most applications should use this:
      * <pre>   {@code
-     *
+     * <p/>
      *     File dexCache = getApplicationContext().getDir("dx", Context.MODE_PRIVATE);
      * }</pre>
      * If the {@code dexCache} is null, this method will consult the {@code
@@ -342,24 +369,15 @@ public final class DexMaker {
      * exception by either providing a non-null value or setting the system
      * property.
      *
-     * @param parent the parent ClassLoader to be used when loading our
-     *     generated types
+     * @param parent   the parent ClassLoader to be used when loading our
+     *                 generated types
      * @param dexCache the destination directory where generated and optimized
-     *     dex files will be written. If null, this class will try to guess the
-     *     application's private data dir.
+     *                 dex files will be written. If null, this class will try to guess the
+     *                 application's private data dir.
      */
     public ClassLoader generateAndLoad(ClassLoader parent, File dexCache) throws IOException {
         if (dexCache == null) {
-            String property = System.getProperty("dexmaker.dexcache");
-            if (property != null) {
-                dexCache = new File(property);
-            } else {
-                dexCache = new AppDataDirGuesser().guess();
-                if (dexCache == null) {
-                    throw new IllegalArgumentException("dexcache == null (and no default could be"
-                            + " found; consider setting the 'dexmaker.dexcache' system property)");
-                }
-            }
+            dexCache = guessDexCache();
         }
 
         byte[] dex = generate();
@@ -371,13 +389,7 @@ public final class DexMaker {
          *
          * TODO: load the dex from memory where supported.
          */
-        File result = File.createTempFile("Generated", ".jar", dexCache);
-        result.deleteOnExit();
-        JarOutputStream jarOut = new JarOutputStream(new FileOutputStream(result));
-        jarOut.putNextEntry(new JarEntry(DexFormat.DEX_IN_JAR_NAME));
-        jarOut.write(dex);
-        jarOut.closeEntry();
-        jarOut.close();
+        File result = spitToJar(dexCache, dex);
         try {
             return (ClassLoader) Class.forName("dalvik.system.DexClassLoader")
                     .getConstructor(String.class, String.class, String.class, ClassLoader.class)
@@ -393,6 +405,63 @@ public final class DexMaker {
         } catch (IllegalAccessException e) {
             throw new AssertionError();
         }
+    }
+
+    /**
+     * Generates a dex file and loads its types into the current process with
+     * the given ClassLoader.
+     *
+     * @param loader   the ClassLoader to be used when loading our generated types
+     * @param dexCache the destination directory where generated and optimized
+     *                 dex files will be written. If null, this class will try to guess the
+     *                 application's private data dir.
+     * @see #generateAndLoad(ClassLoader, java.io.File)
+     */
+    public ClassLoader generateAndLoadWith(final ClassLoader loader, File dexCache) throws IOException {
+        if (dexCache == null) {
+            dexCache = guessDexCache();
+        }
+
+        byte[] dex = generate();
+
+        /*
+         * This implementation currently dumps the dex to the filesystem. It
+         * jars the emitted .dex for the benefit of Gingerbread and earlier
+         * devices, which can't load .dex files directly.
+         *
+         * TODO: load the dex from memory where supported.
+         */
+        File result = spitToJar(dexCache, dex);
+        final Object dexFile;
+        final Method loadClass;
+        try {
+            Class<?> DexFile = Class.forName("dalvik.system.DexFile");
+            dexFile = DexFile.getConstructor(File.class).newInstance(result);
+            loadClass = DexFile.getMethod("loadClass", String.class, ClassLoader.class);
+        } catch (ClassNotFoundException e) {
+            throw new UnsupportedOperationException("load() requires a Dalvik VM", e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e.getCause());
+        } catch (InstantiationException e) {
+            throw new AssertionError();
+        } catch (NoSuchMethodException e) {
+            throw new AssertionError();
+        } catch (IllegalAccessException e) {
+            throw new AssertionError();
+        }
+        return new ClassLoader(loader) {
+            @Override
+            protected Class<?> findClass(String name) throws ClassNotFoundException {
+                Class<?> c;
+                try {
+                    c = (Class<?>) loadClass.invoke(dexFile, name, loader);
+                } catch (Exception e) {
+                    throw new ClassNotFoundException("Class couldn't be loaded: " + name, e);
+                }
+                if (c == null) throw new ClassNotFoundException(name);
+                return c;
+            }
+        };
     }
 
     private static class TypeDeclaration {
